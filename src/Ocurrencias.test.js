85f9591257af2d11ea8dd59588acf3ab4de00a37
Arturo Guzman <arturo.guzman@ucb.edu.bo>
2023-09-05 10:41:05 -0400

    prueba5

--- src/Ocurrencias.test.js
+++ src/Ocurrencias.test.js
@@ -13,4 +13,7 @@ describe("CantOcurrencias", () => {
     it("deberia devolver la cantidad de ocurrencias de todas las palabras en una frase sin improtar mayusculas y minusculas", () => {
         expect(cantDeOcurrencias("hola Hola mundo mundo mundo")).toEqual({ hola: 2, mundo: 3 });
     });
+    it("deberia devolver la cantidad de ocurrencias de todas las palabras en una frase sin improtar mayusculas y minusculas", () => {
+        expect(cantDeOcurrencias("El password de mi cuenta es PASSWORD Es un password de poco cuidado")).toEqual({ el: 1, password: 3, de: 2, mi: 1, cuenta: 1, es: 2, un: 1, poco: 1, cuidado: 1 });
+    });
 });
